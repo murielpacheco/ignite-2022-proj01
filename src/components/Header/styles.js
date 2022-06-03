bd47d1ad4ce37d98baf555cb8edebd96ddf60973
Muriel Damazio <murieldps@gmail.com>
2022-06-03 17:59:10 -0300

    feat: adding css variables and changing css to new variables

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
    width: 100%;
    height: 100px;
-   background-color: #323238;
+   background-color: var(--gray-800);
    
    display: flex;
    justify-content: center;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+   :root {
+      --gray-900: #121214;
+      --gray-800: #202024;
+      --gray-700: #29292e;
+      --gray-600: #323238;
+      --gray-500: #7c7c8a;
+      --gray-400: #8d8d99;
+      --gray-300: #c4c4cc;
+      --gray-100: #e1e1e6;
+      --white: #ffffff;
+      --green-500: #00875f;
+      --green-300: #00B37e;
+
+   }
+   
    * {
       margin: 0;
       padding: 0;
@@ -9,7 +24,9 @@ export const GlobalStyle = createGlobalStyle`
    }
 
    body {
-      background-color: #121214; 
+      background-color: var(--gray-900); 
+      color: var(--gray-300);
+      -webkit-font-smoothing: antialiased;
    }
 
 `;
